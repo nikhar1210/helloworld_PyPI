36f905b13ac8549a3ff750c2fdd98da75fef12a2
Nikhar Shah <nikharshah@Nikhars-MacBook-Pro.local>
2021-02-21 10:45:40 -0500

    version updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='helloworld-nik12',
-    version='0.0.1',
+    version='0.0.2',
     description='say hello',
     py_modules=['helloworld'],
     package_dir={'': 'src'},
