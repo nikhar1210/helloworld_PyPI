eba1f60f2ccb75efc1f82be46932495deb66ef89
Nikhar Shah <nikharshah@Nikhars-MacBook-Pro.local>
2021-02-21 22:39:43 -0500

    helloworld package update 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='helloworld-nik12',
-    version='0.0.2',
+    version='0.0.4',
     description='say hello',
     py_modules=['helloworld'],
     package_dir={'': 'src'},
--- src/helloworld.py
+++ src/helloworld.py
@@ -1,5 +1,7 @@
 
 def say_hello(name=None):
+    if type(name) == int:
+        name = str(name)
     if name is None:
         return "Hello, World!"
     else:
